f855e727299d3579ab2c8ff88cbb057becd7bb27
Aiman2315 <naqviaiman44@gmail.com>
2024-05-24 18:31:17 +0500

    Push to github

---
+++ main.ts
@@ -0,0 +1,48 @@
+#!/usr/bin/env node
+import inquirer from "inquirer"
+
+type Question = {
+    question: string;
+    options: string[];
+    correctAnswerIndex: number;
+  };
+  
+  function startQuiz(questions: Question[]): void {
+    let score = 0;
+  
+    questions.forEach((question, index) => {
+      console.log(`Question ${index + 1}: ${question.question}`);
+      question.options.forEach((option, optionIndex) => {
+        console.log(`${optionIndex + 1}. ${option}`);
+      });
+  
+      const userAnswer = parseInt(prompt("Enter your answer (1, 2, 3, etc.):") || "0");
+      if (userAnswer === question.correctAnswerIndex + 1) {
+        console.log("Correct!");
+        score++;
+      } else {
+        console.log("Incorrect!");
+      }
+    });
+  
+    console.log(`Quiz completed. Your score: ${score}/${questions.length}`);
+  }
+  const questions: Question[] = [
+    {
+      question: "When did Quaid e Azam born?",
+      options: ["25 December 1876", "27 September 1896", "20 August 1870", "20 June 1890"],
+      correctAnswerIndex: 1
+    },
+    {
+      question: "What is 3 * 1?",
+      options: ["3", "4", "5", "6"],
+      correctAnswerIndex: 1
+    },
+    {
+      question: "What is the largest planet in our solar system?",
+      options: ["Mars", "Venus", "Jupiter", "Saturn"],
+      correctAnswerIndex: 2
+    }
+  ];
+  
+  startQuiz(questions);
